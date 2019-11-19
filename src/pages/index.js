4e2d6ccd2b17a9b66afc2e9deea9106967b76697
Callum Macpherson <callummac@protonmail.com>
2019-11-19 14:07:04 +0000

    asdsa

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,6 @@
 import React from "react"
 import { Link, graphql } from "gatsby"
 
-// import Bio from "../components/bio"
-// import Layout from "../components/layout"
-// import SEO from "../components/seo"
-// import { rhythm } from "../utils/typography"
 
 export default ({ data }) => {
   const posts = data.allMarkdownRemark.nodes
